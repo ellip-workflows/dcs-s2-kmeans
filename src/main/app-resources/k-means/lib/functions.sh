cf3e0fac2d2bf21d4e41c233f3fd3c06e86ddb38
Fabrice Brito <fabrice.brito@terradue.com>
2017-07-04 15:08:09 +0200

    Update functions.sh

--- src/main/app-resources/k-means/lib/functions.sh
+++ src/main/app-resources/k-means/lib/functions.sh
@@ -2,27 +2,41 @@
 
 # define the exit codes
 SUCCESS=0
+ERR_NO_URL=5
+ERR_NO_PRD=8
+ERR_GDAL_VRT=10
+ERR_MAP_BANDS=15
+ERR_OTB_BUNDLETOPERFECTSENSOR=20
+ERR_DN2REF_4=25
+ERR_DN2REF_3=25
+ERR_DN2REF_2=25
+ERR_GDAL_VRT2=30
+ERR_GDAL_TRANSLATE=35
+ERR_GDAL_WARP=40
+ERR_GDAL_TRANSLATE=45
+ERR_GDAL_ADDO=50
 ERR_PUBLISH=55
 
-# source the ciop functions (e.g. ciop-log, ciop-getparam)
-source ${ciop_job_include}
-
-###############################################################################
-# Trap function to exit gracefully
-# Globals:
-#   SUCCESS
-#   ERR_PUBLISH
-# Arguments:
-#   None
-# Returns:
-#   None
-###############################################################################
+# add a trap to exit gracefully
 function cleanExit ()
 {
   local retval=$?
   local msg=""
   case "${retval}" in
     ${SUCCESS}) msg="Processing successfully concluded";;
+    ${ERR_NO_URL}) msg="The Sentinel-2 product online resource could not be resolved";;
+    ${ERR_NO_PRD}) msg="The Landsat 8 product online resource could not be retrieved";;
+    ${ERR_GDAL_VRT}) msg="Failed to create the RGB VRT";;
+    ${ERR_MAP_BANDS}) msg="Failed to map RGB bands";;
+    ${ERR_OTB_BUNDLETOPERFECTSENSOR}) msg="Failed to apply BundleToPerfectSensor OTB operator";;
+    ${ERR_DN2REF_4}) msg="Failed to convert DN to reflectance";;
+    ${ERR_DN2REF_3}) msg="Failed to convert DN to reflectance";;
+    ${ERR_DN2REF_2}) msg="Failed to convert DN to reflectance";;
+    ${ERR_GDAL_VRT2}) msg="Failed to create VRT with panchromatic bands";;
+    ${ERR_GDAL_TRANSLATE}) msg="Failed to apply gdal_translate";;
+    ${ERR_GDAL_WARP}) msg="Failed to warp";;
+    ${ERR_GDAL_TRANSLATE2}) msg="Failed to apply gdal_translate";;
+    ${ERR_ADDO}) msg="Failed to create levels";;
     ${ERR_PUBLISH}) msg="Failed to publish the results";;
     *) msg="Unknown error";;
   esac
@@ -32,52 +46,25 @@ function cleanExit ()
 }
 
 
-###############################################################################
-# Log an input string to the log file
-# Globals:
-#   None
-# Arguments:
-#   input reference to log
-# Returns:
-#   None
-###############################################################################
-function log_input()
-{
-  local input=${1}
-  ciop-log "INFO" "processing input: ${input}"
-}
+function main() {
 
-###############################################################################
-# Pass the input string to the next node, without storing it on HDFS
-# Globals:
-#   None
-# Arguments:
-#   input reference to pass
-# Returns:
-#   0 on success
-#   ERR_PUBLISH if something goes wrong 
-###############################################################################
-function pass_next_node()
-{
-  local input=${1}
-  echo "${input}" | ciop-publish -s || return ${ERR_PUBLISH}
-}
+  input=$1
+  
+  cd ${TMPDIR}
 
-###############################################################################
-# Main function to process an input reference
-# Globals:
-#   None
-# Arguments:
-#   input reference to process
-# Returns:
-#   0 on success
-#   ERR_PUBLISH if something goes wrong
-###############################################################################
-function main()
-{
-  local input=${1}
-  # Log the input
-  log_input ${input}
-  # Just pass the input reference to the next node 
-  pass_next_node ${input}
-}
+  num_steps=7
+
+  ciop-log "INFO" "(1 of ${num_steps}) Resolve Sentinel-2 online resource"
+  online_resource="$( opensearch-client ${input} enclosure )"
+  [[ -z ${online_resource} ]] && return ${ERR_NO_URL}
+
+  ciop-log "INFO" "(2 of ${num_steps}) Retrieve Sentinel-2 product from ${online_resource"
+  local_s2="$( ciop-copy ${online_resource} )"
+  [[ -z ${local_s2} ]] && return ${ERR_NO_PRD} 
+
+  
+  
+  # clean-up
+  rm -fr ${local_s2}
+    
+ }
